e9b52af28e1cde412fa2caa0984ca9d36e51ca6f
guimarczewski <65767113+guimarczewski@users.noreply.github.com>
2023-10-17 19:12:35 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 import json
 
 # Configuração do aplicativo
-st.title("Upload de Arquivos CSV para Google Cloud Storage")
+st.title("Upload de Arquivos para Google Cloud Storage")
 uploaded_credentials = st.file_uploader("Faça o upload do arquivo de credenciais JSON")
 uploaded_file = st.file_uploader("Faça o upload do arquivo CSV")
 
@@ -31,30 +31,21 @@ if uploaded_credentials is not None:
                 storage_client = storage.Client(credentials=credentials)
 
                 st.success("Credenciais carregadas com sucesso!")
-
-                if uploaded_file is not None:
-                    # Verifique se o arquivo é um CSV
-                    if uploaded_file.type == 'application/vnd.ms-excel':
-                        df = pd.read_csv(uploaded_file)
-
-                        # Exiba as 10 primeiras linhas do arquivo
-                        st.subheader("Visualização das 10 Primeiras Linhas:")
-                        st.dataframe(df.head(10))
-
-                        # Botão para enviar o arquivo
-                        if st.button("Enviar para o Google Cloud Storage"):
-                            # Resto do código para envio do arquivo
-                            # ...
-
-                            st.success("Upload concluído com sucesso!")
-
-                    else:
-                        st.error("O arquivo deve ser um CSV.")
-                else:
-                    st.info("Carregue um arquivo CSV para visualizar e enviar.")
-            else:
-                st.error("O arquivo de credenciais está faltando informações necessárias.")
         except Exception as e:
             st.error(f"Erro ao carregar as credenciais: {e}")
+
+if uploaded_file is not None:
+    # Exiba as 10 primeiras linhas do arquivo
+    st.subheader("Visualização das 10 Primeiras Linhas:")
+    df = pd.read_csv(uploaded_file)
+    st.dataframe(df.head(10))
+
+    # Botão para enviar o arquivo
+    if st.button("Enviar para o Google Cloud Storage"):
+        if storage_client is not None:
+            # Resto do código para envio do arquivo
+            # ...
+
+            st.success("Upload concluído com sucesso!")
 
 # Fim do seu código Streamlit
